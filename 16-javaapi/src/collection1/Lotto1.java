1b7ff27578294a0ad65a8132ce1e69cf66a6a96d
seonghynu <seonghynu9724@naver.com>
2021-07-07 18:05:16 +0900

    추가

--- 16-javaapi/src/collection1/Lotto1.java
+++ 16-javaapi/src/collection1/Lotto1.java
@@ -3,7 +3,6 @@ package collection1;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 
 public class Lotto1 {
 
@@ -13,25 +12,20 @@ public class Lotto1 {
 		// 6개의 숫자를 뽑습니다.
 		// 순서를 정렬해서 출력하도록 만들어주세요.
 		// ArrayList의 정렬은 .sort()대신
-		// collections.sort(리스트);를 이용해 정렬합니다.
-		
-		Random r = new Random();
-		
+		// collections.sort(리스트);를 이용해 정렬합니다.	
 		List<Integer> lotto = new ArrayList<>();
-		
-		int Number = r.nextInt(45) +1;
-		
-		lotto.add(Number);
-		lotto.add(Number);
-		lotto.add(Number);
-		lotto.add(Number);
-		lotto.add(Number);
-		lotto.add(Number);
-		
-		Collections.sort(lotto);
-		
-		System.out.println("추첨된 번호:" + lotto);
-		System.out.println("당첨번호:" + Number);
+		int getNum;
+		while(lotto.size() !=6) { // 6개가 아니면 돌리고 6개가 되면 탈출해라.
+			getNum = (int)(Math.random() * 45) +1; 
+			if(!lotto.contains(getNum)) {
+				// contains는 없을때 false, 있을때 true 이지만
+				// !를 사용하여 없는숫자가 나올경우 true가 되어 실행.
+				lotto.add(getNum);
+			}
+		}
+		Collections.sort(lotto); // 정렬해주는 입력값.
+		System.out.println("추첨된 번호:" + lotto); // 실행문
+
 	}
 
 }
